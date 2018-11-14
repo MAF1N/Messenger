fdf8c98c55b859a001e91776a3d0c83c10f7691f
Nikita Yarovoy <nikita_yarovoy@ukr.net>
2018-11-14 23:28:51 +0200

    fix logging and work with db with db pooling

--- app.js
+++ app.js
@@ -12,6 +12,11 @@ const client = MongoClient(mongodbConnectionString);
 const jwt = require('jsonwebtoken');
 
 var clients = {};
+var db;
+client.connect(function(err) {
+    db = client.db(dbName);
+    app.listen(port, () => console.log(`Messenger listening on port ${port}!`));
+});
 
 var createNicknameIndex = require('./logic/create-nickname-index');
 var createUser = require('./logic/user');
@@ -20,7 +25,7 @@ var findByNickName = require('./logic/find-by-nickname');
 var getChat = require('./logic/get-chat');
 var getCurrentUserChats = require('./logic/current-user-chats');
 var addChatMessage = require('./logic/add-chat-message');
-
+var formatDate = require('./logic/format-date');
 app.use(jsonParser);
 
 app.use(function(err, req, res, next) {
@@ -30,180 +35,136 @@ app.use(function(err, req, res, next) {
 
 app.post('/register', (req, res) =>{
     try {
-        client.connect(function(err) {
-            if (!err){
-                console.info(`[${Date.now().toString()}] Registering new user`);
-                const db = client.db(dbName);
-                const user = createUser(req.body);
-                if (user == null){
-                    console.info(`[${Date.now().toString()}] User was not created`);
-                    return;
-                }
-                db.collection('users').find(user).limit(1).toArray((err, docs) => {
-                    console.info(`[${Date.now().toString()}] Searching for exact user`);
-                    if(!docs && err!=null || err == null && docs.length == 0){
-                        db.collection('users').insertOne(user, function(err, r) {
-                            if(err && r.insertedCount == 1){
-                                console.log("Successfully added user");
-                            }
-                        });
-                    } else if (docs.length > 0){
-                        console.info(`[${Date.now().toString()}] User already exists`);
-                        client.close();
-                        res.status(200).send("User already exists");
-                    } else if (err){
-                        console.log(err);
+        console.info(`[${formatDate(new Date())}] Registering new user`);
+        const user = createUser(req.body);
+        if (user == null){
+            console.info(`[${formatDate(new Date())}] User was not created`);
+            return;
+        }
+        db.collection('users').find(user).limit(1).toArray((err, docs) => {
+            console.info(`[${ formatDate(new Date())}] Searching for exact user`);
+            if(!docs && err!=null || err == null && docs.length == 0){
+                db.collection('users').insertOne(user, function(err, r) {
+                    if(err && r.insertedCount == 1){
+                        console.log("Successfully added user");
                     }
-    
-                    db.close();
                 });
+            } else if (docs.length > 0){
+                console.info(`[${ formatDate(new Date())}] User already exists`);
+                client.close();
+                res.status(200).send("User already exists");
+            } else if (err){
+                console.log(err);
             }
         });
         res.status(200).send();
     }
     catch(ex){
-        console.error(`[${Date.now().toString()}] Register issue: ${ex}`);
+        console.error(`[${ formatDate(new Date())}] Register issue: ${ex}`);
         res.status(400).send(ex);
     }
 });
 
 app.post('/login', (req, res) => {
-    console.info(`[${Date.now().toString()}] Login`);
+    console.info(`[${ formatDate(new Date())}] Login`);
     const user = createUser(req.body);
-    client.connect(function(err) {
-        if (err){
-            console.error(`[${Date.now().toString()}] ${err}`);
-        }
-        const db = client.db(dbName);
-        db.collection('users').find(user).limit(1).toArray(function(err, docs) {         
-            if(err){
-                console.error(err);
-            }else {
-                if (docs.length == 1){
-                    console.info(`[${Date.now().toString()}] User was found`);
-                    res.status(200).send(jwt.sign(user, "supersecret"));
-                } else {
-                    console.info(`[${Date.now().toString()}] User was not found`);
-                    res.status(400).send();
-                }
+    db.collection('users').find(user).limit(1).toArray(function(err, docs) {         
+        if(err){
+            console.error(err);
+        }else {
+            if (docs.length == 1){
+                console.info(`[${ formatDate(new Date())}] User was found`);
+                res.status(200).send(jwt.sign(user, "supersecret"));
+            } else {
+                console.info(`[${ formatDate(new Date())}] User was not found`);
+                res.status(400).send();
             }
-            db.close();
-        });
+        }
     });
-    
 });
 
 app.use('/user/info', tokenchecker);
 app.get('/user/info',(req, res) => {
-    console.info(`[${Date.now().toString()}] User info called`);
+    console.info(`[${ formatDate(new Date())}] User info called`);
     let searchQuery = jwt.verify(req.get('token'), "supersecret").nickname;
-    client.connect(function(err) {
-        const db = client.db(dbName);
-        if (err){
-            console.error(`[${Date.now().toString()}] ${err}`);
-        }
-        findByNickName(db, searchQuery, function(docs) {
-            if (docs && docs.length > 0){
-                let retVal = docs.find(x => x.nickname == searchQuery);
-                console.info(`[${Date.now().toString()}] returning user info: ${retVal}`);
-                res.status(200).json(retVal).send();
-            } else {
-                console.info(`[${Date.now().toString()}] There is no such a user`);
-                res.status(404).send("Noone with such nickname was found.");
-            }
-            db.close();          
-        });    
-    });
+    findByNickName(db, searchQuery, function(docs) {
+        if (docs && docs.length > 0){
+            let retVal = docs.find(x => x.nickname == searchQuery);
+            console.info(`[${ formatDate(new Date())}] returning user info: ${retVal}`);
+            res.status(200).json(retVal).send();
+        } else {
+            console.info(`[${ formatDate(new Date())}] There is no such a user`);
+            res.status(404).send("Noone with such nickname was found.");
+        }            
+    });    
 })
 
 app.use('/users/search', tokenchecker);
 app.get('/users/search', (req, res) => {
     const searchQuery = req.query.query;
     console.info("Searching user with "+ searchQuery);
-    client.connect(function(err) {
-        if (err){
-            console.error(`[${Date.now().toString()}] ${err}`);
-        }
-        const db = client.db(dbName);
-        let decoded = jwt.verify(req.get('token'), "supersecret").nickname;
-        console.info(`[${Date.now().toString()}] Requested by ${decoded}`);
-        findByNickName(db, searchQuery, function(docs) {
-            if (docs && docs.length > 0){
-                docs = docs.filter(x => x.nickname.toLowerCase() != decoded.toLowerCase());
-                res.status(200).json(docs).send();
-            } else {
-                console.info(`[${Date.now().toString()}] [user/search] There is noone with such nickname `);
-                res.status(404).send("Noone with such nickname was found.");
-            }   
-            db.close();       
-        });    
-    });
+    let decoded = jwt.verify(req.get('token'), "supersecret").nickname;
+    console.info(`[${ formatDate(new Date())}] Requested by ${decoded}`);
+    findByNickName(db, searchQuery, function(docs) {
+        if (docs && docs.length > 0){
+            docs = docs.filter(x => x.nickname.toLowerCase() != decoded.toLowerCase());
+            res.status(200).json(docs).send();
+        } else {
+            console.info(`[${ formatDate(new Date())}] [user/search] There is noone with such nickname `);
+            res.status(404).send("Noone with such nickname was found.");
+        }        
+    });    
 });
 
 app.use('/chat', tokenchecker);
 app.post('/chat', function(req, res){
-    client.connect((err)=> {
-        if (err){
-            console.error(`[${Date.now().toString()}] ${err}`);
-        }
-        const db = client.db(dbName);
-        const decoded = jwt.verify(req.get('token'), "supersecret");
-        console.info(`[${Date.now().toString()}] Chat`);
-        console.info(`[${Date.now().toString()}] Requested by ${decoded.nickname} with ${req.body.title}`);
-        getChat(db, decoded.nickname, req.body.nickname, req.body.title, (result) => {
-            res.json(result[0]).send();
-            db.close();
-        });
+    const decoded = jwt.verify(req.get('token'), "supersecret");
+    console.info(`[${ formatDate(new Date())}] Chat`);
+    console.info(`[${ formatDate(new Date())}] Requested by ${decoded.nickname} with ${req.body.title}`);
+    getChat(db, decoded.nickname, req.body.nickname, req.body.title, (result) => {
+        res.json(result[0]).send();
+         
     });
 });
 
 app.use('/chats', tokenchecker);
 app.get('/chats', function(req, res) {
-    client.connect((err) => {
-        if (err){
-            console.error(`[${Date.now().toString()}] ${err}`);
-        }
-        const db = client.db(dbName);
-        const decoded = jwt.verify(req.get('token'), "supersecret");
-        console.info(`[${Date.now().toString()}] Chats`);
-        console.info(`[${Date.now().toString()}] Requsted by ${decoded.nickname}`);
-        getCurrentUserChats(db, decoded.nickname, (result) => {
-            res.json(result).send();
-            db.close();
-        });
+    const decoded = jwt.verify(req.get('token'), "supersecret");
+    console.info(`[${ formatDate(new Date())}] Chats`);
+    console.info(`[${ formatDate(new Date())}] Requsted by ${decoded.nickname}`);
+    getCurrentUserChats(db, decoded.nickname, (result) => {
+        res.json(result).send();
+         
     });
 });
 
 app.use('/send/message', tokenchecker);
 app.use('/send/message', function(req, res){
-    client.connect((err)=> {
-        const db = client.db(dbName);
-        const decoded = jwt.verify(req.get('token'), "supersecret");
-        console.info(`[${Date.now().toString()}] Sending a message by ${decoded.nickname} to ${req.body.nickname}`);
-        addChatMessage(db, decoded.nickname, req.body.nickname, req.body.message, (result) => {
-            if (result){
-                if(clients[req.body.nickname]){
-                    console.info(`[${Date.now().toString()}] The client with such nickname is currently online`);
-                    console.info(`[${Date.now().toString()}] Sending a message via WS`);
-                    clients[req.body.nickname].send(result);
-                }
-                res.status(200).json(result).send();
+    const decoded = jwt.verify(req.get('token'), "supersecret");
+    console.info(`[${ formatDate(new Date())}] Sending a message by ${decoded.nickname} to ${req.body.nickname}`);
+    addChatMessage(db, decoded.nickname, req.body.nickname, req.body.message, (result) => {
+        if (result){
+            if(clients[req.body.nickname]){
+                console.info(`[${ formatDate(new Date())}] The client with such nickname is currently online`);
+                console.info(`[${ formatDate(new Date())}] Sending a message via WS`);
+                clients[req.body.nickname].send(result);
             }
-            db.close();
-        });
+            res.status(200).json(result).send();
+        }
+         
     });
 });
 
 app.ws('/chat-ws', function(ws, req) {
     ws.on('authenticate', function(msg) {
-        console.info(`[${Date.now().toString()}] WS: Authenticated`);
+        console.info(`[${ formatDate(new Date())}] WS: Authenticated`);
         token = JSON.parse(msg).token;
         let decoded = jwt.decode(token);
         clients[decoded.nickname] = ws;
     });
 
     ws.on('close', function(){ 
-        console.info(`[${Date.now().toString()}] WS: Closed`);
+        console.info(`[${ formatDate(new Date())}] WS: Closed`);
         // for (const key in clients) {
         //     if (clients.hasOwnProperty(key)) {
         //         const element = clients[key];
@@ -216,9 +177,7 @@ app.ws('/chat-ws', function(ws, req) {
     console.log('socket');
 });
 
-app.listen(port, () => console.log(`Messenger listening on port ${port}!`));
-
 process.on('uncaughtException', (error) =>{
-    console.error(`[${Date.now().toString()}] UncaughtException: ${error}`);
+    console.error(`[${ formatDate(new Date())}] UncaughtException: ${error}`);
     app.removeAllListeners();
 });
